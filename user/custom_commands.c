61bebb156834b1cfde06fb07adc70c5c67c2f155
con-f-use <con-f-use@gmx.net>
2016-08-16 20:12:17 +0200

    Fixes

--- user/custom_commands.c
+++ user/custom_commands.c
@@ -1,7 +1,7 @@
 //Copyright 2015 <>< Charles Lohr, see LICENSE file.
 
 #include <commonservices.h>
-#include "common/mystuff.h"
+#include "mystuff.h"
 
 int ICACHE_FLASH_ATTR CustomCommand(char * buffer, int retsize, char *pusrdata, unsigned short len) {
 	char * buffend = buffer;
